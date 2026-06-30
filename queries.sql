a67de2f64b1d8703d9df4ad7643b2dbddc6772f1
Rakibstack <rh6039511@gmail.com>
2026-06-30 16:44:56 +0600

    feat: complete football ticket booking database design and SQL queries

--- queries.sql
+++ queries.sql
@@ -22,8 +22,47 @@ or full_name ilike '%Haque%'
 -- ===================================================== 
 
 select booking_id,user_id,match_id ,
-coalesce(payment_status,'Action Required') as systematic_status
+coalesce(payment_status,'Action Required')
+as systematic_status
 from bookings 
 where payment_status is null 
+
+-- =====================================================
+-- Query 4:
+-- Retrieve Bookings with user and match details
+-- ===================================================== 
+
+
+select booking_id,full_name,fixture,total_cost
+from bookings
+inner join users using(user_id)
+inner join matches using(match_id)
+
+-- =====================================================
+-- Query 5:
+-- Display a comprehensive list using LEFT JOIN 
+-- ===================================================== 
+
+select u.user_id,u.full_name,b.booking_id
+from users u
+left join bookings b on u.user_id = b.user_id
+
+-- =====================================================
+-- Query 6:
+-- Find all ticket bookings where the total cost is strictly higher than the average cost of all ticket bookings. 
+-- ===================================================== 
+
+select booking_id,match_id,total_cost
+ from bookings
+where total_cost > (select avg(total_cost) from bookings)
+
+-- =====================================================
+-- Query 7:
+-- Find the top 2 most expensive matches based on base ticket price. 
+-- ===================================================== 
+
+select match_id,fixture,base_ticket_price 
+from matches 
+order by base_ticket_price desc limit 2 offset 1
 
 
